eff4545ce2c9783866b698fdd6e5db577d94b78c
Stefan Sträter <stefan.straeter@gmail.com>
2026-02-02 15:24:58 +0100

    refactor: getData and postData

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,25 +1,30 @@
-let usersData = [];
 const BASE_URL = "https://join-7c944-default-rtdb.europe-west1.firebasedatabase.app/";
 
-async function init() {
-    await loadData();
+
+async function getData(path = "") {
+    try {
+        let response = await fetch(BASE_URL + path + ".json");
+        let responseToJson = await response.json();
+        return responseToJson;
+    } catch (error) {
+        console.error("Error loading data:", error);
+        return null;
+    }
 }
 
-async function loadData(path = "") {
-    let response = await fetch(BASE_URL + path + ".json");
-    let responseToJson = await response.json();
-
-    usersData = responseToJson || [];
-    console.log(usersData);
-}
 
 async function postData(path = "users", data = {}) {
-    let response = await fetch(BASE_URL + path + ".json", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data)
-    });
-    return await response.json();
+    try {
+        let response = await fetch(BASE_URL + path + ".json", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        });
+        return await response.json();
+    } catch (error) {
+        console.error("Error loading data:", error);
+    }
 }
+
